0c3b55c974a77c3411f1dee2ad4df442026d54ec
Hugo Orlando Gonzalez <hugo.orlando2050@gmail.com>
2023-03-12 22:09:01 -0300

    parametro de rutas

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.responses import HTMLResponse
 app = FastAPI()
 
 app.title = "Mi aplicación con  FastAPI"
-app.version = "0.0.1" 
+app.version = "0.0.1"
 
 movies = [
     {
@@ -13,6 +13,14 @@ movies = [
 		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
 		"year": "2009",
 		"rating": 7.8,
+		"category": "Acción"
+	},
+    {
+		"id": 2,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2016",
+		"rating": 8.8,
 		"category": "Acción"
 	}
 ]
@@ -25,3 +33,25 @@ async def root():
 @app.get('/movies', tags=['movies'])
 async def get_movies():
     return movies
+
+
+@app.get('/movies/{id}', tags=['movies'])
+async def get_movie_by_id(id: int):
+    for item in movies:
+         if item['id'] == id:
+              return item
+         
+    return {"message": "Movie not found"}
+
+
+	
+
+
+# def search_movie(id):
+#     movie = list(filter(lambda item: item['id'] == id, movies))
+
+# 	try:
+# 		return movie[0]
+# 	except:
+# 		return {"message": "Movie not found"}
+
