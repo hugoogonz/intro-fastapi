fd722d7869d255eb2176f10aa633854b2bf7ff04
Hugo Orlando Gonzalez <hugo.orlando2050@gmail.com>
2023-03-12 21:57:05 -0300

    metodo get

--- main.py
+++ main.py
@@ -2,6 +2,25 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+app.title = "Mi aplicación con  FastAPI"
+app.version = "0.0.1" 
+
+movies = [
+    {
+		"id": 1,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	}
+]
+
+@app.get('/', tags=['home'])
 async def root():
     return 'Hi from FastAPI'
+
+
+@app.get('/movies', tags=['movies'])
+async def get_movies():
+    return movies
